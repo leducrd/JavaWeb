ee632af199045018deb985769119d48a2de91363
leducrd <leducrd@gmail.com>
2017-04-20 15:14:05 -0500

    Add Movie class

---
+++ Movies_rleduc6/src/main/java/edu/cvtc/web/model/Movie.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package edu.cvtc.web.model;
+
+import java.io.Serializable;
+
+/**
+ * @author ryanleduc
+ *
+ */
+public class Movie implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String title;
+	private String director;
+	private int lengthInMinutes;
+	
+	public Movie() {
+		
+	}
+	
+	public Movie(String title, String director, int lengthInMinutes) {
+		this.title = title;
+		this.director = director;
+		this.lengthInMinutes = lengthInMinutes;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDirector() {
+		return director;
+	}
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+	public int getLengthInMinutes() {
+		return lengthInMinutes;
+	}
+
+	public void setLengthInMinutes(int lengthInMinutes) {
+		this.lengthInMinutes = lengthInMinutes;
+	}
+	
+	
+	
+	
+}
