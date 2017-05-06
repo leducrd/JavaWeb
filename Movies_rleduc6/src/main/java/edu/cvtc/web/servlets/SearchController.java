4fce751ee8dbacefc1c111948d961124552a0c2f
leducrd <leducrd@gmail.com>
2017-05-05 22:17:52 -0500

    Add SearchController and behavior to allow for searching by title

---
+++ Movies_rleduc6/src/main/java/edu/cvtc/web/servlets/SearchController.java
@@ -0,0 +1,69 @@
+package edu.cvtc.web.servlets;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+
+import edu.cvtc.web.model.Movie;
+import edu.cvtc.web.util.WorkbookUtility;
+
+/**
+ * Servlet implementation class SearchController
+ */
+@WebServlet("/Search")
+public class SearchController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String target = null;
+		
+		try {
+			
+			final String filepath = getServletContext().getRealPath(WorkbookUtility.INPUT_FILE);
+			final File inputFile = new File(filepath);
+			final List<Movie> movies = WorkbookUtility.retrieveMoviesFromWorkbook(inputFile);
+			
+			final String title = request.getParameter("title");
+			
+			final List<Movie> filteredMovies = movies.stream()
+					.filter((movie) -> movie.getTitle().equalsIgnoreCase(title)).collect(Collectors.toList());
+			
+			request.setAttribute("movies", filteredMovies);
+			
+			target = "view-all.jsp";
+			
+			
+		} catch (InvalidFormatException e) {
+			
+			e.printStackTrace();
+			
+			request.setAttribute("message", "The workbook file has an invalid format.");
+			
+			target = "error.jsp";
+		}
+		
+		request.getRequestDispatcher(target).forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
