440637e8e71646a5727a9da30632f07ec2766776
leducrd <leducrd@gmail.com>
2017-05-16 13:08:28 -0500

    Modified SearchController to use DAO interface rather than workbook

--- Movies_rleduc6/WebContent/addMovie.jsp
+++ Movies_rleduc6/WebContent/addMovie.jsp
@@ -18,13 +18,13 @@
         
         <div class="container">
           <form action="AddMovie" method="post">
-            <label for="title"><strong>First Name: </strong></label>
+            <label for="title"><strong>Title: </strong></label>
             <input name="title"><br>
             
-            <label for="director"><strong>Last Name: </strong></label>
+            <label for="director"><strong>Director: </strong></label>
             <input name="director"><br>
             
-            <label for="lengthInMinutes"><strong>Age: </strong></label>
+            <label for="lengthInMinutes"><strong>Length (In Minutes): </strong></label>
             <input name="lengthInMinutes"><br>
             
             <input type="submit" value="Add Movie">
--- Movies_rleduc6/src/main/java/edu/cvtc/web/servlets/SearchController.java
+++ Movies_rleduc6/src/main/java/edu/cvtc/web/servlets/SearchController.java
@@ -1,6 +1,5 @@
 package edu.cvtc.web.servlets;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -11,10 +10,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-
+import edu.cvtc.web.dao.MovieDao;
+import edu.cvtc.web.dao.impl.MovieDaoException;
+import edu.cvtc.web.dao.impl.MovieDaoImpl;
 import edu.cvtc.web.model.Movie;
-import edu.cvtc.web.util.WorkbookUtility;
 
 /**
  * Servlet implementation class SearchController
@@ -32,9 +31,8 @@ public class SearchController extends HttpServlet {
 		
 		try {
 			
-			final String filepath = getServletContext().getRealPath(WorkbookUtility.INPUT_FILE);
-			final File inputFile = new File(filepath);
-			final List<Movie> movies = WorkbookUtility.retrieveMoviesFromWorkbook(inputFile);
+			final MovieDao movieDao = new MovieDaoImpl();
+			final List<Movie> movies = movieDao.retrieveMovies();
 			
 			final String title = request.getParameter("title");
 			
@@ -46,11 +44,11 @@ public class SearchController extends HttpServlet {
 			target = "view-all.jsp";
 			
 			
-		} catch (InvalidFormatException e) {
+		} catch (MovieDaoException e) {
 			
 			e.printStackTrace();
 			
-			request.setAttribute("message", "The workbook file has an invalid format.");
+			request.setAttribute("message", "Cannot access database to search.");
 			
 			target = "error.jsp";
 		}
